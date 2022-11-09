50e1f5a812ca1c0c420fbdc2f643aa0f8c8ad0d8
Lu Vazquez <lucero.muva313@gmail.com>
2022-11-09 16:07:30 -0500

    updates styles

---
+++ script.js
@@ -0,0 +1,19 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+    /*var logo = document.getElementById('logo');
+    logo.height = 25;
+    logo.width = 25;*/
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+    /*var logo = document.getElementById('logo');
+    logo.height = 35;
+    logo.width = 35;*/
+  }
+}
+
